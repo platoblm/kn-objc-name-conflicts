ed62d6e04ee90ae4b14093e95abd492f64a50297
Plato Balomenakis <plato.balomenakis@gmail.com>
2018-12-13 23:22:36 +0000

    init repo

---
+++ src/commonMain/kotlin/com/example/first/View.kt
@@ -0,0 +1,6 @@
+package com.example.first
+
+data class View(
+    val field1: String,
+    val field2: List<String>
+)
---
+++ src/commonMain/kotlin/com/example/second/View.kt
@@ -0,0 +1,6 @@
+package com.example.second
+
+data class View(
+    val field3: String,
+    val field4: List<String>
+)
